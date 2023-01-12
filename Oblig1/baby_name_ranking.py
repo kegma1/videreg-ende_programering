7814a3dc155aa4525d9a73e0094781e13cd72dc9
kegma1 <kennetam02@gmai.com>
2023-01-12 12:09:51 +0100

    done i think

--- Oblig1/baby_name_ranking.py
+++ Oblig1/baby_name_ranking.py
@@ -2,17 +2,22 @@ import urllib.request
 
 def main():
     year = input("Enter the year: ")
-    
-    gender = input("Enter the gender: ")
+    if int(year) < 2001 or int(year) > 2010:
+        print("Only 2001-2010 is supported")
+        return
+    gender = input("Enter the gender: ").upper()
+    if gender != "F" and gender != "M":
+        print("Your gender in invalid, only 'F' or 'M'")
+        return
     name = input("Enter the name: ")
     
     data_for_year = urllib.request.urlopen(f"https://liveexample.pearsoncmg.com/data/babynameranking{year}.txt").readlines()
 
-    rank = find_rank(data_for_year, name, gender).strip()
+    rank = find_rank(data_for_year, name, gender)
 
     if rank != None:
         gender_str = "Girl" if gender == "F" else "Boy"
-        print(f"{gender_str} name {name} is ranked #{rank} in year {year}")
+        print(f"{gender_str} name {name} is ranked #{rank.strip()} in year {year}")
     else:
         print("not found")
             
